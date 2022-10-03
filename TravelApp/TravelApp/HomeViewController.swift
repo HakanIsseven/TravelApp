cb4d8cbddd26ef19bfd8616390ddd86d900c3858
Hakan Isseven <95025428+HakanIsseven@users.noreply.github.com>
2022-10-04 01:17:01 +0300

    App UI updated
    Search sayfası ui yapıldı

---
+++ TravelApp/TravelApp/HomeViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  TravelApp
+//
+//  Created by Hakan İşseven on 30.09.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var topPickArticlesCollectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+
+
+
+}
+
+private extension HomeViewController {
+    
+    func setupUI() {
+        topPickArticlesCollectionView.delegate = self
+        topPickArticlesCollectionView.dataSource = self
+        
+        registerCell()
+    }
+    func registerCell() {
+        topPickArticlesCollectionView.register(.init(nibName: "TopArticlesCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "TopArticlesCollectionViewCell")
+    }
+    
+}
+
+
+
+extension HomeViewController: UICollectionViewDataSource  {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TopArticlesCollectionViewCell", for: indexPath) as! TopArticlesCollectionViewCell
+        
+        cell.layer.cornerRadius = 6
+        
+        
+        return cell
+    }
+    
+    
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout  {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return.init(width: (topPickArticlesCollectionView.frame.width * 0.64), height: topPickArticlesCollectionView.frame.height)
+        
+        
+    }
+    
+    
+    
+}
---
+++ TravelApp/TravelApp/TopArticlesCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TopArticlesCollectionViewCell.swift
+//  TravelApp
+//
+//  Created by Hakan İşseven on 3.10.2022.
+//
+
+import UIKit
+
+class TopArticlesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+    
+    
+}
